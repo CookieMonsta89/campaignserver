2b4c61e2d94687acc45875f6c9a9c6d2314e3163
CookieMonsta89 <jlstan06@hotmail.com>
2019-09-15 22:50:14 -0400

    added google oauth packages and keys

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //root file
 
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;//exports multiple properties but only care about "Strategy"
 const app = express();
 
+passport.use(new GoogleStrategy());//creates a new instance of the GoogleStrategy
 
-app.get('/', (req, res) => {
-    res.send({ hi:'what is up' });
-})
+
 
 const PORT = process.env.PORT || 5000; //creates dynamic variable. Uses the port that heroku gives to us during runtime
 app.listen(PORT);
